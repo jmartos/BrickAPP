86938d8b58d797317743f57546b14e69aa592209
Javier Martos Oneale <javier.martos.oneale@gmail.com>
2016-05-16 12:29:50 +0200

    adding devise and user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get '/', to: 'site#home'
   get '/contact', to: 'site#contact'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#Seed users
+10.times do
+  email = [ Faker::Internet.email ].sample
+  name = [ Faker::Company.name ].sample
+  address = [ Faker::Address.street_address ].sample
+  zip_code = [ Faker::Address.zip_code ].sample
+  city = [ Faker::Address.city ].sample
+  country = [ Faker::Address.country ].sample
+  phone_number_1 = [ Faker::PhoneNumber.phone_number ].sample
+  phone_number_2 = [ Faker::PhoneNumber.cell_phone ].sample
+  fax_number = [ Faker::PhoneNumber.phone_number ].sample
+  web_page = [ Faker::Internet.url ].sample
+  image = [ Faker::Company.logo ].sample
+  password = 123456
+  password_confirmation = 123456
+  
+  puts "Creating #{email}"
+  User.create(
+    email: email, 
+    name: name, 
+    address: address, 
+    zip_code: zip_code, 
+    city: city, 
+    country: country,
+    phone_number_1: phone_number_1,
+    phone_number_2: phone_number_2,
+    fax_number: fax_number,
+    web_page: web_page,
+    image: image,  
+    password: password,
+    password_confirmation: password_confirmation,
+    ) 
+end
+
