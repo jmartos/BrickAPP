5b78667e0f4960495c1d008ac2025d4cde58dc8e
Javier Martos Oneale <javier.martos.oneale@gmail.com>
2016-05-17 13:05:34 +0200

    home page

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,14 +1,14 @@
-$(document).ready(function () {
-  $("#register").click(function () {
-    window.location.href = "/users/sign_up";
-  })
-  $("#login").click(function () {
-    window.location.href = "/users/sign_in";
-  })
-  $("#edit_profile").click(function () {
-    window.location.href = "/users/edit";
-  })
-})
+// $(document).ready(function () {
+//   $("#register").click(function () {
+//     window.location.href = "/users/sign_up";
+//   })
+//   $("#login").click(function () {
+//     window.location.href = "/users/sign_in";
+//   })
+//   $("#edit_profile").click(function () {
+//     window.location.href = "/users/edit";
+//   })
+// })
 
 
 
