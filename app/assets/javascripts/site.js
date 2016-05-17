aa0c4711fdc411843e00d60a2d42b9f3e605d416
Javier Martos Oneale <javier.martos.oneale@gmail.com>
2016-05-17 20:36:08 +0200

    users data views

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,33 +1,34 @@
-// $(document).ready(function () {
-//   $("#register").click(function () {
-//     window.location.href = "/users/sign_up";
-//   })
-//   $("#login").click(function () {
-//     window.location.href = "/users/sign_in";
-//   })
-//   $("#edit_profile").click(function () {
-//     window.location.href = "/users/edit";
-//   })
-// })
+$(document).ready(function () {
+  // $("#register").click(function () {
+  //   window.location.href = "/users/sign_up";
+  // })
+  // $("#login").click(function () {
+  //   window.location.href = "/users/sign_in";
+  // })
+$("#edit_profile").click(function () {
+  window.location.href = "/users/edit";
+})
+})
 
 
-function init_map(){
-  var myOptions = {
-    zoom:16,
-    center:new google.maps.LatLng(40.4382915,-3.681471800000054),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);
-  marker = new google.maps.Marker({
-    map: map,
-    position: new google.maps.LatLng(40.4382915,-3.681471800000054)});
-  infowindow = new google.maps.InfoWindow({
-    content:'<strong>BrickAPP</strong><br>Nuñez de Balboa 120, Madrid<br>'
-  });
-  google.maps.event.addListener(marker, 'click', function(){
-    infowindow.open(map,marker);
-  });
-  infowindow.open(map,marker);
-}
-google.maps.event.addDomListener(window, 'load', init_map);
+
+// function init_map(){
+//   var myOptions = {
+//     zoom:16,
+//     center:new google.maps.LatLng(40.4382915,-3.681471800000054),
+//     mapTypeId: google.maps.MapTypeId.ROADMAP
+//   };
+//   map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);
+//   marker = new google.maps.Marker({
+//     map: map,
+//     position: new google.maps.LatLng(40.4382915,-3.681471800000054)});
+//   infowindow = new google.maps.InfoWindow({
+//     content:'<strong>BrickAPP</strong><br>Nuñez de Balboa 120, Madrid<br>'
+//   });
+//   google.maps.event.addListener(marker, 'click', function(){
+//     infowindow.open(map,marker);
+//   });
+//   infowindow.open(map,marker);
+// }
+// google.maps.event.addDomListener(window, 'load', init_map);
 
--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,24 +1,29 @@
 class SiteController < ApplicationController
 
-  layout :resolve_layout
+  # before_action :resolve_layout
 
   def home
     if user_signed_in? 
       redirect_to "/profile"
+    else
+      render(:layout => "layouts/site")
     end
   end
 
   def contact
+    render(:layout => "layouts/application")
   end
 
   def support
   end
 
-private
+  private
 
-  def resolve_layout
-    if user_signed_in? 
-      "application"
-    end
-  end
+  # def resolve_layout
+  #   if user_signed_in? 
+  #     "application"
+  #   else
+  #     "site"
+  #   end
+  # end
 end
