c2b833e15f73527b3096ee2bdc2aed8f5ac52264
Javier Martos Oneale <javier.martos.oneale@gmail.com>
2016-05-24 16:34:52 +0200

    fix buttons working

--- app/models/supplier_review.rb
+++ app/models/supplier_review.rb
@@ -1,3 +1,7 @@
 class SupplierReview < ActiveRecord::Base
   belongs_to :supplier
+
+  validates :projected_budget, presence: { message: "is required" }, numericality: { :greater_than => 0, :less_than_or_equal_to => 10, message: "is required" }
+  validates :final_budget, presence: { message: "is required" }, numericality: { :greater_than => 0, :less_than_or_equal_to => 10, message: "is required" }
+  validates :user_valoration, presence: { message: "is required" }, numericality: { :greater_than => 0, :less_than_or_equal_to => 10, message: "is required" }
 end
