6feae7c187fda8fa4ba4ec022ea23ae62a5ca807
Javier Martos Oneale <javier.martos.oneale@gmail.com>
2016-05-24 16:48:03 +0200

    refactor reviews

--- app/models/supplier_review.rb
+++ app/models/supplier_review.rb
@@ -1,7 +1,7 @@
 class SupplierReview < ActiveRecord::Base
   belongs_to :supplier
 
-  validates :projected_budget, presence: { message: "is required" }, numericality: { :greater_than => 0, :less_than_or_equal_to => 10, message: "is required" }
-  validates :final_budget, presence: { message: "is required" }, numericality: { :greater_than => 0, :less_than_or_equal_to => 10, message: "is required" }
-  validates :user_valoration, presence: { message: "is required" }, numericality: { :greater_than => 0, :less_than_or_equal_to => 10, message: "is required" }
+  validates :projected_budget, presence: { message: "is required" }, numericality: { message: "is required" }
+  validates :final_budget, presence: { message: "is required" }, numericality: { message: "is required" }
+  validates :user_valoration, presence: { message: "is required" }, numericality: { :greater_than => 0, :less_than_or_equal_to => 10, message: "your valoration must be between 0 and 10" }
 end
